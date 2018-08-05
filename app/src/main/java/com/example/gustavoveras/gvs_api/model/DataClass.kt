b64b0ac79439c3cac92f863939200c683d593d8a
gustavoveras <gustavoveras2013@gmail.com>
2018-08-05 17:44:34 -0300

    Criando as classes de modelo com definiçao do retorno das requisiçoes da api

---
+++ app/src/main/java/com/example/gustavoveras/gvs_api/model/DataClass.kt
@@ -0,0 +1,13 @@
+package com.example.gustavoveras.gvs_api.model
+
+data class Movie (val title : String,
+                  val episodeId : Int,
+                  val characters : MutableList<Character>)
+
+data class Character(val name : String,
+                     val gender : String){
+
+    override fun toString(): String {
+        return "${name} / ${gender}"
+    }
+}
