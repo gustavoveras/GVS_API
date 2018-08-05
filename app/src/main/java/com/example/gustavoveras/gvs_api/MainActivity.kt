f8a30bd3423ab47a4da53f3a90323e1351b82215
gustavoveras <gustavoveras2013@gmail.com>
2018-08-05 18:10:25 -0300

    Correção de alguns pontos

--- app/src/main/java/com/example/gustavoveras/gvs_api/MainActivity.kt
+++ app/src/main/java/com/example/gustavoveras/gvs_api/MainActivity.kt
@@ -2,11 +2,33 @@ package com.example.gustavoveras.gvs_api
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import com.example.gustavoveras.gvs_api.model.api.StarWarsApi
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var listView : ListView
+    lateinit var movieAdapter : ArrayAdapter<String>
+    var movies = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        listView = ListView(this)
+        setContentView(listView)
+        movieAdapter = ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, movies)
+        listView.adapter = movieAdapter
+
+        val api = StarWarsApi()
+        api.loadMovies()?.subscribeOn(Schedulers.io())?.observeOn(AndroidSchedulers.mainThread())?.subscribe ({ movie ->
+            movies.add("${movie.title} -- ${movie.episodeId}")
+        }, { e ->
+            e.printStackTrace()
+        },{
+            movieAdapter.notifyDataSetChanged()
+        })
     }
 }
